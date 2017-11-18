040a21cc3fbe4301fee8034183e717a5e1ce6717
Dustin <e2755699@gmail.com>
2017-11-19 02:07:33 +0800

    add testCase  : InputQueueIs_1_2_3_andTwoTill()

--- Kata20171119_CheckoutTills/Kata.cs
+++ Kata20171119_CheckoutTills/Kata.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Kata20171119_CheckoutTills
@@ -7,8 +8,56 @@ namespace Kata20171119_CheckoutTills
     public class Kata
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InputQueueIs_1_2_3_4_andOneTill()
         {
+            //arrange
+            var queue = new[] {1, 2, 3, 4};
+            var tills = 1;
+            var expect = 10;
+            //act
+            int actual = new CheckoutTills().CaculateRequestTime(queue, tills);
+            //assert
+            Assert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void InputQueueIs_2_3_4_5_andOneTill()
+        {
+            AssertShouldBe(new[] { 2, 3, 4, 5 }, 1, 14);
+        }
+
+        [TestMethod]
+        public void InputQueueIs_1_2_andTwoTill()
+        {
+            AssertShouldBe(new[] { 1, 2}, 2, 2);
+        }
+
+        [TestMethod]
+        public void InputQueueIs_1_2_3_andTwoTill()
+        {
+            AssertShouldBe(new[] { 1, 2 ,3}, 2, 4);
+        }
+
+
+        private static void AssertShouldBe(int[] queue, int tills, int expect)
+        {
+            //act
+            int actual = new CheckoutTills().CaculateRequestTime(queue, tills);
+            //assert
+            Assert.AreEqual(expect, actual);
+        }
+    }
+
+    public class CheckoutTills
+    {
+        public int CaculateRequestTime(int[] queue, int tills)
+        {
+            if (tills == 1)
+            {
+                return queue.Aggregate((result, a) => result + a);
+            }
+
+            return Math.Max(queue[0], queue[1]);
         }
     }
 }
