595018cdcb483ea64baa410c1bc7fd99945a13cc
Dustin <e2755699@gmail.com>
2017-11-20 00:04:54 +0800

    change logic of production but fail the test cases

--- Kata20171119_CheckoutTills/Kata.cs
+++ Kata20171119_CheckoutTills/Kata.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -56,19 +57,92 @@ namespace Kata20171119_CheckoutTills
 
     public class CheckoutTills
     {
-        public int CaculateRequestTime(int[] queue, int tills)
+        public int CaculateRequestTime(int[] queue, int tillsNumber)
         {
-            if (tills == 1)
-            {
-                return queue.Aggregate((result, a) => result + a);
-            }
+            var tills = new TillCenter(queue, tillsNumber);
+            return tills.StartWorking();
+        }
+    }
 
-            if (queue.Length == 2)
-            {
-                return Math.Max(queue[0], queue[1]);
-            }
+    public class TillCenter
+    {
+        private List<Till> tills;
 
-            return Math.Max(Math.Min(queue[0], queue[1]) + queue[2], Math.Max(queue[0], queue[1]));
+        private int[] queue;
+        private List<Till> freeTills = new List<Till>();
+        private int position;
+
+        private int totalRequestTime
+        {
+            get { return tills.Max(till => till.totalRequestTime); }
+        }
+
+        private bool isComplete
+        {
+            get { return position == queue.Length; }
+        }
+
+        public TillCenter(int[] queue, int tillsNumber)
+        {
+            this.queue = queue;
+
+            for (int i = 0; i < tillsNumber; i++)
+            {
+                tills.Add(new Till());
+            }
+        }
+
+        public int StartWorking()
+        {
+            while (true)
+            {
+                if (isFreeTill())
+                {
+                    if (isComplete)
+                    {
+                        return totalRequestTime;
+                    }
+
+                    freeTills.ForEach(till => till.AddRequest(queue[position]));
+                }
+
+                reduceOneRequestTime();
+            }
+        }
+
+        private void reduceOneRequestTime()
+        {
+            tills.ForEach(till => till.ReduceRequestTime());
+        }
+
+        private bool isFreeTill()
+        {
+            return freeTills.Any();
+        }
+    }
+
+    public class Till
+    {
+        private int remaainingTime { get; set; }
+
+        public int totalRequestTime { get; set; }
+
+        public void AddRequest(int requestTime)
+        {
+            IsFree = false;
+            remaainingTime = requestTime;
+            totalRequestTime += requestTime;
+        }
+        
+        public bool IsFree { get; internal set; }
+
+        public void ReduceRequestTime()
+        {
+            remaainingTime--;
+            if (remaainingTime == 0)
+            {
+                IsFree = true;
+            }
         }
     }
 }
